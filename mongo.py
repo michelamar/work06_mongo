7f5ae18dd1ef226a7b3018da136f950ab1f0679a
Andrew <awong21@stuy.edu>
2018-02-27 22:25:51 -0500

    form working

--- mongo.py
+++ mongo.py
@@ -1,12 +1,10 @@
 import pymongo
 import urllib2
 import json
-from Flask import flask
+from flask import Flask, render_template, request, session, redirect
 
 connection = pymongo.MongoClient('homer.stuy.edu')
-db = connection.neverGonnaGifYouUp
-db.dropDatabase()
-
+connection.drop_database('neverGonnaGifYouUp')
 db = connection.neverGonnaGifYouUp
 collection = db.gifs
 u = urllib2.urlopen("http://api.giphy.com/v1/gifs/search?q=happy&api_key=dc6zaTOxFJmzC")
@@ -15,12 +13,31 @@ data = json.loads(x)
 data = data['data']
 collection.insert_many(data)
 
+list_of_gifs = {}
+url_to_display = ''
+for gif in collection.find({'type':'gif'},{'_id':0, 'title':1, 'url':1}):
+    #print gif['title']
+    list_of_gifs[gif['title']] = gif['url']
+
+#print list_of_gifs
+
+app = Flask(__name__)
+
 @app.route('/')
 def root():
-    db.collection.find({
-    return render_template('form.html')
+    return render_template('form.html', list_of_gifs = list_of_gifs)
+    
+@app.route('/display')
+def display():
+    url_to_display = request.args['gif_name']
+    return redirect('/gif')
 
 @app.route('/gif', methods=['POST', 'GET'])
 def gif():
-    gifname = request.form['gif']
-    
+    print url_to_display
+    return render_template('gif.html', url = url_to_display)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
